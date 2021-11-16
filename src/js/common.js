9807d7f56af8690c06f806f7580ae3175ebcaa87
t-orange <t-orange@yandex.ru>
2021-11-16 05:47:41 +0300

    change pug files

--- src/js/common.js
+++ src/js/common.js
@@ -1,23 +1,23 @@
 
 $(document).ready(function () {
 
-	// Mask
-	$("input[name='phone']").mask("+7(999) 999-99-99");
+  // Mask
+  $("input[name='phone']").mask("+7(999) 999-99-99");
 
-	// Fancybox
-	$('.js-link-modal').fancybox({
-		animationEffect : "zoom-in-out",
-		slideClass : 'modal-close'
-	});
+  // Fancybox
+  $('.js-link-modal').fancybox({
+    animationEffect : "zoom-in-out",
+    slideClass : 'modal-close'
+  });
 
-	// Slider
-	$('.slider').slick({
-		prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"></button>',
-		nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"></button>',
-		infinite: true,
-		speed: 300,
-		slidesToShow: 2,
-		slidesToScroll: 1
-	});
-	
+  // Slider
+  $('.slider').slick({
+    prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"></button>',
+    nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"></button>',
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  });
+  
 });
